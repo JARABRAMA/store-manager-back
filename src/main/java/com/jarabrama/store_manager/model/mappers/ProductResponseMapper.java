5bfd3c37d2f6ef320bc29b5354e827f71fb00f0d
brayan <brayanjaraba2006@gmail.com>
2026-02-27 06:59:31 -0500

    feat: create mapper for product response dto

---
+++ src/main/java/com/jarabrama/store_manager/model/mappers/ProductResponseMapper.java
@@ -0,0 +1,12 @@
+package com.jarabrama.store_manager.model.mappers;
+
+import com.jarabrama.store_manager.model.dtos.ProductResponse;
+import com.jarabrama.store_manager.model.entitties.Product;
+
+public class ProductResponseMapper {
+
+  public static ProductResponse fromEntity(Product product) {
+    return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getUrlImage(),
+        product.getPrice(), product.getStock());
+  }
+}
