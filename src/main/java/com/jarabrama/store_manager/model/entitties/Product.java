93b393d4d25b6d80b1bb5e82c15f739caa3550f6
brayan <brayanjaraba2006@gmail.com>
2026-02-27 06:43:01 -0500

    feat: create product entity

--- src/main/java/com/jarabrama/store_manager/model/entitties/Product.java
+++ src/main/java/com/jarabrama/store_manager/model/entitties/Product.java
@@ -1,5 +1,31 @@
 package com.jarabrama.store_manager.model.entitties;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
   
+  @Id
+  private UUID id; 
+
+  private String name; 
+  private String description;
+  private int price; 
+  private int stock; 
+  private String urlImage; 
+  private LocalDateTime createdAt; 
 }
