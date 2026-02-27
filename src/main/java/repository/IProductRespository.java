e2e37caab246c27416a202394373ab9efbdae8a1
brayan <brayanjaraba2006@gmail.com>
2026-02-27 07:02:39 -0500

    fix: add steriotype to repository

--- src/main/java/repository/IProductRespository.java
+++ src/main/java/repository/IProductRespository.java
@@ -3,9 +3,11 @@ package repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.jarabrama.store_manager.model.entitties.Product;
 
+@Repository
 public interface IProductRespository extends JpaRepository<Product, UUID> {
 
 }
