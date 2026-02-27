79aabca41aad62c6ce4a9431c5e1e625271f35c6
brayan <brayanjaraba2006@gmail.com>
2026-02-27 06:59:11 -0500

    feat: create product reponse dto

---
+++ src/main/java/com/jarabrama/store_manager/model/dtos/ProductResponse.java
@@ -0,0 +1,9 @@
+package com.jarabrama.store_manager.model.dtos;
+
+import java.util.UUID;
+
+public record ProductResponse(
+  UUID id, String name, String description, 
+  String urlImage, int price, int stock
+) {
+}
