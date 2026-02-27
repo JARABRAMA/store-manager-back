3f8df50569508609a0178fb36957cb7a70cfb836
brayan <brayanjaraba2006@gmail.com>
2026-02-27 06:56:17 -0500

    fix: conceptual error at jpa repository

--- src/main/java/repository/IProductRespository.java
+++ src/main/java/repository/IProductRespository.java
@@ -1,12 +1,11 @@
 package repository;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jarabrama.store_manager.model.entitties.Product;
 
-public interface IProductRespository extends JpaRepository<UUID, Product> {
-  List<Product> findProducts();
+public interface IProductRespository extends JpaRepository<Product, UUID> {
+
 }
