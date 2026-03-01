bcf315efc51c0edda5d39d3ff3f3f63ee13d6153
brayan <brayanjaraba2006@gmail.com>
2026-03-01 15:34:52 -0500

    fix: add categories to findbyid endpoint

--- src/main/java/com/jarabrama/store_manager/repository/IProductRespository.java
+++ src/main/java/com/jarabrama/store_manager/repository/IProductRespository.java
@@ -1,6 +1,7 @@
 package com.jarabrama.store_manager.repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,22 +14,29 @@ import com.jarabrama.store_manager.model.entitties.Product;
 @Repository
 public interface IProductRespository extends JpaRepository<Product, UUID> {
 
-    @Query("""
-            SELECT DISTINCT p FROM Product p
-            LEFT JOIN FETCH p.categories
-            LEFT JOIN p.categories c
-            WHERE (UPPER(p.name) LIKE UPPER(CONCAT('%', :search, '%'))
-            OR UPPER(p.description) LIKE UPPER(CONCAT('%', :search, '%')))
-            AND UPPER(c.name) = UPPER(:category)
-            """)
-    List<Product> findAll(@Param("search") String search, @Param("category") String category);
+	@Query("""
+			SELECT DISTINCT p FROM Product p
+			LEFT JOIN FETCH p.categories
+			LEFT JOIN p.categories c
+			WHERE (UPPER(p.name) LIKE UPPER(CONCAT('%', :search, '%'))
+			OR UPPER(p.description) LIKE UPPER(CONCAT('%', :search, '%')))
+			AND UPPER(c.name) = UPPER(:category)
+			""")
+	List<Product> findAll(@Param("search") String search, @Param("category") String category);
 
-    @Query("""
-            SELECT DISTINCT p FROM Product p
-            LEFT JOIN FETCH p.categories
-            WHERE UPPER(p.name) LIKE UPPER(CONCAT('%', :search, '%'))
-            OR UPPER(p.description) LIKE UPPER(CONCAT('%', :search, '%'))
-            """)
-    List<Product> findAll(@Param("search") String search);
+	@Query("""
+			SELECT DISTINCT p FROM Product p
+			LEFT JOIN FETCH p.categories
+			WHERE UPPER(p.name) LIKE UPPER(CONCAT('%', :search, '%'))
+			OR UPPER(p.description) LIKE UPPER(CONCAT('%', :search, '%'))
+			""")
+	List<Product> findAll(@Param("search") String search);
+
+	@Query("""
+			SELECT DISTINCT p FROM Product p
+			LEFT JOIN FETCH p.categories
+			WHERE p.id = :id
+			""")
+	Optional<Product> findAllById(@Param("id") UUID id);
 
 }
