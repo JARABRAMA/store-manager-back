ff75d5e3813a13bd40305244a14deace710ce828
brayan <brayanjaraba2006@gmail.com>
2026-03-01 07:12:32 -0500

    fix: remove unnused imports

--- src/main/java/com/jarabrama/store_manager/model/entitties/Category.java
+++ src/main/java/com/jarabrama/store_manager/model/entitties/Category.java
@@ -1,14 +1,9 @@
 package com.jarabrama.store_manager.model.entitties;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
